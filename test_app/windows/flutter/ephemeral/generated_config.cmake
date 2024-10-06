a5cb73dc30bfd3d426ab8da545bfb71f097864dc
Vivs <22vivienegarcia@gmail.com>
2024-10-06 21:49:34 +0800

    Initial commit

---
+++ test_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Flutter\\src\\flutter_windows_3.24.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\3RD YEAR\\App Dev\\projects_vs_flutter\\test_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Flutter\\src\\flutter_windows_3.24.3-stable\\flutter"
+  "PROJECT_DIR=D:\\3RD YEAR\\App Dev\\projects_vs_flutter\\test_app"
+  "FLUTTER_ROOT=D:\\Flutter\\src\\flutter_windows_3.24.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\3RD YEAR\\App Dev\\projects_vs_flutter\\test_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\3RD YEAR\\App Dev\\projects_vs_flutter\\test_app"
+  "FLUTTER_TARGET=D:\\3RD YEAR\\App Dev\\projects_vs_flutter\\test_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\3RD YEAR\\App Dev\\projects_vs_flutter\\test_app\\.dart_tool\\package_config.json"
+)
