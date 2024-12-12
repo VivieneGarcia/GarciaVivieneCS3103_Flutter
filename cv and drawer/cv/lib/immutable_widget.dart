8fc4952d2dc4ae85ff1371f8c95ffa51ef6795a1
Vivs <22vivienegarcia@gmail.com>
2024-12-12 16:38:12 +0800

    ediits

--- cv and drawer/cv/lib/immutable_widget.dart
+++ cv and drawer/cv/lib/immutable_widget.dart
@@ -36,94 +36,75 @@ class ImmutableWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 30, 0, 20),
-                height: 130,
-                width: 130,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: const Color.fromARGB(255, 180, 180, 180),
-                  border: Border.all(
-                    color: const Color.fromARGB(147, 44, 44, 44),
-                    width: 4,
-                  ),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                CircleAvatar(
+                  radius: 65,
+                  backgroundColor: const Color.fromARGB(255, 180, 180, 180),
+                  backgroundImage:
+                      imageBytes != null ? MemoryImage(imageBytes!) : null,
+                  child: imageBytes == null
+                      ? Text(
+                          name[0].toUpperCase(),
+                          style: const TextStyle(fontSize: 50),
+                        )
+                      : null,
                 ),
-                child: imageBytes == null
-                    ? Center(
-                        child: Text(
-                          name.isNotEmpty ? name[0].toUpperCase() : '',
-                          style: const TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.w500,
-                            color: Color.fromARGB(255, 65, 65, 65),
-                          ),
-                        ),
-                      )
-                    : ClipOval(
-                        child: Image.memory(
-                          imageBytes!,
-                          fit: BoxFit.cover,
-                          width: 130,
-                          height: 130,
-                        ),
-                      ),
-              ),
-              Positioned(
-                right: 2,
-                top: 30,
-                child: Container(
-                  width: 35,
-                  height: 35,
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 71, 71, 71),
-                    shape: BoxShape.circle,
-                  ),
-                  child: IconButton(
-                    onPressed: () => _pickImage(context),
-                    icon: const Icon(
-                      Icons.edit,
-                      color: Colors.white,
+                Positioned(
+                  right: 2,
+                  top: 2,
+                  child: Container(
+                    width: 35,
+                    height: 35,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 71, 71, 71),
+                      shape: BoxShape.circle,
                     ),
-                    iconSize: 25,
-                    padding: const EdgeInsets.all(2),
+                    child: IconButton(
+                      onPressed: () => _pickImage(context),
+                      icon: const Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      ),
+                      iconSize: 25,
+                      padding: const EdgeInsets.all(2),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Text(
-            name,
-            style: const TextStyle(fontSize: 21, fontWeight: FontWeight.w600),
-          ),
-          Text(number, style: const TextStyle(fontSize: 16)),
-          Text(email, style: const TextStyle(fontSize: 16)),
-          const SizedBox(height: 10),
-          Container(
-              width: 350,
-              height: 200,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 239, 239, 239),
-                border: Border.all(
-                  color: const Color.fromARGB(255, 143, 143, 143),
-                  width: 2,
+              ],
+            ),
+            Text(
+              name,
+              style: const TextStyle(fontSize: 21, fontWeight: FontWeight.w600),
+            ),
+            Text(number, style: const TextStyle(fontSize: 16)),
+            Text(email, style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 10),
+            Container(
+                width: 350,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 239, 239, 239),
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 143, 143, 143),
+                    width: 2,
+                  ),
                 ),
-              ),
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Text(motto,
-                    style: TextStyle(
-                      fontSize: 15,
-                    )),
-              ))
-        ],
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(motto,
+                      style: TextStyle(
+                        fontSize: 15,
+                      )),
+                ))
+          ],
+        ),
       ),
     );
   }
